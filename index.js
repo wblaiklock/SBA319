aeb527463f149b40bff0950e418c285a57a6b5f6
wblaiklock <wblaiklock@gmail.com>
2025-01-16 18:54:52 -0500

    Added routes

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-//Data
-const starterFruits = require('./config/seed');         
 //Models
 const Posts = require('./models/posts');
 const Users = require('./models/users');
@@ -26,18 +23,46 @@ app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes); 
 app.use('/api/images', imageRoutes); 
 
-//Home route
-app.get('/', async (req,res) => {
-    await res.send('Home page');
-})
 
 
+
+
+
+const fs = require('fs');
+//Getters
+app.get("/", (req, res) => {
+    const options = {
+      title: "Rendering Views with Express",
+      content:
+        "Testerino",
+    };
+    
+    res.render("index", options);
+  });
+
+  //Template
+app.engine("index", (filePath, options, callback) => { // define the template engine
+    fs.readFile(filePath, (err, content) => {
+      if (err) return callback(err);
+  
+      // Here, we take the content of the template file,
+      // convert it to a string, and replace sections of
+      // it with the values being passed to the engine.
+      const rendered = content
+        .toString()
+        .replaceAll("#title#", `${options.title}`)
+        .replace("#content#", `${options.content}`);
+      return callback(null, rendered);
+    });
+  });
+
+
+  
+
+app.set("views", "./views"); // specify the views directory
+app.set("view engine", "index"); // register the template engine
 
 //Listen
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`)
 })
-
-
-
-
